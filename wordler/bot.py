1bd6a38a2b974db775e89766a1928e6b900ce406
Jakob Blaschke <jakobblaschke@yahoo.de>
2026-02-28 18:10:59 +0100

    Create extract_stats function

--- wordler/bot.py
+++ wordler/bot.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import sqlite3
 from dotenv import load_dotenv
 
 from telegram import Update, ForceReply
@@ -20,6 +21,70 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
+# --- DATABASE SETUP ---
+def init_db():
+    with sqlite3.connect("wordle_stats.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+    CREATE TABLE IF NOT EXISTS wordles (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER,
+                    username TEXT,
+                    timestamp TEXT,
+                    wordle_id INTEGER,
+                    hard_mode BOOLEAN,
+                    solved BOOLEAN,
+                    guesses_needed INTEGER,
+                    guesses TEXT
+                    )
+                    ''')
+        conn.commit()
+
+
+def save_wordle(
+        user_id: int,
+        username: str,
+        timestamp: str,
+        wordle_id: int,
+        hard_mode: bool,
+        solved: bool,
+        guesses_needed: int,
+        guesses: str) -> None:
+    """Save a result to the wordles table."""
+    with sqlite3.connect('wordle_stats.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO wordles (user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            (user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses)
+        )
+        conn.commit()
+
+
+def extract_stats(user_id: int, include_unsolved: bool) -> dict:
+    """Extract some stats for a user."""
+    with sqlite3.connect('wordles_stats.db') as conn:
+        cursor = conn.cursor()
+
+        # define query with ? to prevent sql injection
+        if include_unsolved:
+            query = "SELECT guesses_needed FROM wordles WHERE user_id = ?"
+        else:
+            query = "SELECT guesses_needed FROM wordles WHERE user_id = ? AND solved = 1"
+        cursor.execute(query, (user_id,))
+        results = cursor.fetchall()
+
+        guess_list = [row[0] for row in results]
+
+    stats = {
+        'guess_list': guess_list,
+        'no_of_guesses': len(guess_list),
+        'total_number_of_guesses': sum(guess_list),
+        'average_score': sum(guess_list) / len(guess_list)
+    }
+
+    return stats
+
+
 # --- COMMAND HANDLERS ---
 # command /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -34,15 +99,33 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 # --- MESSAGE HANDLER ---
 async def reply_wordle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """If the message contains the Wordle Share string, extract the stats from it and send them back."""
+    # get the text from the received update
     text = update.effective_message.text
-    parse_result = parse_wordle_share_text(text)
-    if parse_result:
-        answer = stats_reply_message(parse_result)
+
+    # try to parse the text and extract the stats
+    result = parse_wordle_share_text(text)
+    if result:
+        # save result to database
+        user = update.effective_user
+        user_id = user.id
+        username = user.username or user.first_name
+        timestamp = update.message.date.isoformat()
+        wordle_id = result['wordle_id']
+        hard_mode = result['hard_mode']
+        solved = result['solved']
+        guesses_needed = result['guesses_needed']
+        guesses = '\n'.join(result['guesses'])
+
+        save_wordle(user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses)
+
+        answer = stats_reply_message(result)
         await update.message.reply_text(answer)
 
 
 def main() -> None:
     """Start the Bot."""
+    init_db()
+
     app = Application.builder().token(api_key).build()
 
     # Command Handlers
