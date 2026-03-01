6318b2b7624f1786978b8b24ca7f1404537feef0
Jakob Blaschke <jakobblaschke@yahoo.de>
2026-03-01 20:28:14 +0100

    Move database funcs to own file

--- src/bot.py
+++ src/bot.py
@@ -1,85 +1,20 @@
 import os
 import logging
-import sqlite3
 
 from telegram import Update, ForceReply
 from telegram.ext import Application, MessageHandler, CommandHandler, filters, ContextTypes
 
 from text_handling import parse_wordle_share_text, stats_reply_message
+from database_connection import init_db, save_wordle, extract_stats
 
 
 # Env variables
 api_key = os.getenv("WORDLER_API_KEY")
-database_path = os.getenv("DATABASE_PATH")
 # Enable logging
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 # set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
-
-
-# --- DATABASE SETUP ---
-def init_db():
-    with sqlite3.connect(database_path) as conn:
-        cursor = conn.cursor()
-        cursor.execute('''
-    CREATE TABLE IF NOT EXISTS wordles (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    user_id INTEGER,
-                    username TEXT,
-                    timestamp TEXT,
-                    wordle_id INTEGER,
-                    hard_mode BOOLEAN,
-                    solved BOOLEAN,
-                    guesses_needed INTEGER,
-                    guesses TEXT
-                    )
-                    ''')
-        conn.commit()
-
-
-def save_wordle(
-        user_id: int,
-        username: str,
-        timestamp: str,
-        wordle_id: int,
-        hard_mode: bool,
-        solved: bool,
-        guesses_needed: int,
-        guesses: str) -> None:
-    """Save a result to the wordles table."""
-    with sqlite3.connect(database_path) as conn:
-        cursor = conn.cursor()
-        cursor.execute(
-            "INSERT INTO wordles (user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-            (user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses)
-        )
-        conn.commit()
-
-
-def extract_stats(user_id: int, include_unsolved: bool) -> dict:
-    """Extract some stats for a user."""
-    with sqlite3.connect(database_path) as conn:
-        cursor = conn.cursor()
-
-        # define query with ? to prevent sql injection
-        if include_unsolved:
-            query = "SELECT guesses_needed FROM wordles WHERE user_id = ?"
-        else:
-            query = "SELECT guesses_needed FROM wordles WHERE user_id = ? AND solved = 1"
-        cursor.execute(query, (user_id,))
-        results = cursor.fetchall()
-
-        guess_list = [row[0] for row in results]
-
-    stats = {
-        'guess_list': guess_list,
-        'no_of_guesses': len(guess_list),
-        'total_number_of_guesses': sum(guess_list),
-        'average_score': sum(guess_list) / len(guess_list)
-    }
-
-    return stats
 
 
 # --- COMMAND HANDLERS ---
---
+++ src/database_connection.py
@@ -0,0 +1,69 @@
+import os
+import sqlite3
+
+# Env variables
+database_path = os.getenv("DATABASE_PATH")
+
+
+# --- DATABASE SETUP ---
+def init_db():
+    with sqlite3.connect(database_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+    CREATE TABLE IF NOT EXISTS wordles (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER,
+                    username TEXT,
+                    timestamp TEXT,
+                    wordle_id INTEGER,
+                    hard_mode BOOLEAN,
+                    solved BOOLEAN,
+                    guesses_needed INTEGER,
+                    guesses TEXT
+                    )
+                    ''')
+        conn.commit()
+
+
+def save_wordle(
+        user_id: int,
+        username: str,
+        timestamp: str,
+        wordle_id: int,
+        hard_mode: bool,
+        solved: bool,
+        guesses_needed: int,
+        guesses: str) -> None:
+    """Save a result to the wordles table."""
+    with sqlite3.connect(database_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO wordles (user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            (user_id, username, timestamp, wordle_id, hard_mode, solved, guesses_needed, guesses)
+        )
+        conn.commit()
+
+
+def extract_stats(user_id: int, include_unsolved: bool) -> dict:
+    """Extract some stats for a user."""
+    with sqlite3.connect(database_path) as conn:
+        cursor = conn.cursor()
+
+        # define query with ? to prevent sql injection
+        if include_unsolved:
+            query = "SELECT guesses_needed FROM wordles WHERE user_id = ?"
+        else:
+            query = "SELECT guesses_needed FROM wordles WHERE user_id = ? AND solved = 1"
+        cursor.execute(query, (user_id,))
+        results = cursor.fetchall()
+
+        guess_list = [row[0] for row in results]
+
+    stats = {
+        'guess_list': guess_list,
+        'no_of_guesses': len(guess_list),
+        'total_number_of_guesses': sum(guess_list),
+        'average_score': sum(guess_list) / len(guess_list)
+    }
+
+    return stats
