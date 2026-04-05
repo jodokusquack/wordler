54a226c5a5a72c6e67787d8d6024f8946ffeca76
Jakob Blaschke <jakobblaschke@yahoo.de>
2026-04-05 18:27:55 +0200

    Update test to use fixture object

--- tests/models/user_test.py
+++ tests/models/user_test.py
@@ -20,19 +20,19 @@ class TestUser:
         # user gets created in test_user fixture
 
         # Assert: Check if the user was created
-        db_user = test_db_session.query(User).filter_by(username="test_user").first()
+        db_user = test_db_session.query(User).filter_by(username=test_user.username).first()
         assert db_user is not None
-        assert db_user.telegram_user_id == 12345
+        assert db_user.telegram_user_id == test_user.telegram_user_id
 
     def test_get_user_by_telegram_id(self, test_db_session, test_user):
-        user = get_user_by_telegram_id(test_db_session, 12345)
+        user = get_user_by_telegram_id(test_db_session, test_user.telegram_user_id)
 
         assert user is not None
         assert user.username == "test_user"
 
     def test_delete_user(self, test_db_session, test_user):
-        result = delete_user(test_db_session, 12345)
+        result = delete_user(test_db_session, test_user.telegram_user_id)
         assert result is True
 
-        user = get_user_by_telegram_id(test_db_session, 12345)
+        user = get_user_by_telegram_id(test_db_session, test_user.telegram_user_id)
         assert user is None
